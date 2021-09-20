d7d60642da7e2257309b5903c2c2e6738b8db2e6
param <param759@gmail.com>
2021-09-19 23:38:00 -0400

    first attempt

---
+++ src/test/java/Bol/Rome/dolTest.java
@@ -0,0 +1,19 @@
+package Bol.Rome;
+
+import org.testng.annotations.Test;
+
+public class dolTest {
+
+	@Test
+	public void dame()
+	
+	{
+		System.out.println("work to be done");
+	}
+	
+	@Test(groups= {"smoke"})
+	public void fol()
+	{
+		System.out.println("del done");
+	}
+}
---
+++ src/test/java/Bol/Rome/itlTest.java
@@ -0,0 +1,12 @@
+package Bol.Rome;
+
+import org.testng.annotations.Test;
+
+public class itlTest {
+
+	@Test(groups= {"smoke"})
+	public void game()
+	{
+		System.out.println("dom is done");
+	}
+}
